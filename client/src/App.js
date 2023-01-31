1d0e10baf79a24422d9984fa5a79dc25ca301cba
EsotericZ <cjsand03@gmail.com>
2023-01-31 10:25:05 -0700

    basic react seyup

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './components/home/Home';
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route
+          path='/'
+          element={<Home />}
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
