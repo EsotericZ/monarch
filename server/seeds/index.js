8e42fb370dc28eb56d0fc47339fbef7ab5331d8a
unknown <cjsand03@gmail.com>
2023-05-02 08:17:30 -0600

    seedS

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,7 +1,7 @@
-// const seedJobs = require('./job-seeds');
-// const seedMaintenance = require('./maintenance-seeds');
+const seedJobs = require('./job-seeds');
+const seedMaintenance = require('./maintenance-seeds');
 const seedShip = require('./ship-seeds');
-// const seedUsers = require('./user-seeds');
+const seedUsers = require('./user-seeds');
 
 const sequelize = require('../config');
 
@@ -9,17 +9,17 @@ const seedAll = async () => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
 
-    // await seedJobs();
-    // console.log('\n----- JOBS SEEDED -----\n');
+    await seedJobs();
+    console.log('\n----- JOBS SEEDED -----\n');
 
-    // await seedMaintenance();
-    // console.log('\n----- MAINTENANCE SEEDED -----\n');
+    await seedMaintenance();
+    console.log('\n----- MAINTENANCE SEEDED -----\n');
 
     await seedShip();
     console.log('\n----- SHIPPING SEEDED -----\n');
 
-    // await seedUsers();
-    // console.log('\n----- USERS SEEDED -----\n');
+    await seedUsers();
+    console.log('\n----- USERS SEEDED -----\n');
 
     process.exit(0);
 };
