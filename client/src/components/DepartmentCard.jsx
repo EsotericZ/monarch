1d8afbc8545f9ecd4bc0885fdbb1085f1f57cdcf
unknown <cjsand03@gmail.com>
2024-03-02 12:11:54 -0700

    departments

---
+++ client/src/components/DepartmentCard.jsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom';
+
+import Card from 'react-bootstrap/Card';
+
+import flaserIcon from '../images/flaserIcon.png';
+import formingIcon from '../images/formingIcon.png';
+import laserIcon from '../images/laserIcon.png';
+import punchIcon from '../images/punchIcon.png';
+import sawIcon from '../images/sawIcon.png';
+import shearIcon from '../images/shearIcon.png';
+import slaserIcon from '../images/slaserIcon.png';
+import tlaserIcon from '../images/tlaserIcon.png';
+
+export const DepartmentCard = ({ area }) => {
+    let backgroundColor;
+    switch (area.areaType) {
+        case 'production':
+            backgroundColor = 'lightgreen';
+            break;
+        case 'programming':
+            backgroundColor = 'red';
+            break;
+        case 'tools':
+            backgroundColor = 'yellow';
+            break;
+        default:
+            backgroundColor = 'white'; // Default background color
+            break;
+    }
+
+    return (
+        <Link to={area.link}>
+            <Card style={{ width: '250px', margin: '10px', padding: '10px', backgroundColor }}>
+                {area.image == 'flaser' ? (
+                    <Card.Img variant="top" src={flaserIcon} />
+                ) : area.image == 'forming' ? (
+                    <Card.Img variant="top" src={formingIcon} />
+                ) : area.image == 'laser' ? (
+                    <Card.Img variant="top" src={laserIcon} />
+                ) : area.image == 'punch' ? (
+                    <Card.Img variant="top" src={punchIcon} />
+                ) : area.image == 'saw' ? (
+                    <Card.Img variant="top" src={sawIcon} />
+                ) : area.image == 'shear' ? (
+                    <Card.Img variant="top" src={shearIcon} />
+                ) : area.image == 'slaser' ? (
+                    <Card.Img variant="top" src={slaserIcon} />
+                ) : area.image == 'tlaser' ? (
+                    <Card.Img variant="top" src={tlaserIcon} />
+                ) : null
+                }
+                <Card.Body>
+                    <Card.Title>{area.area}</Card.Title>
+                    <Card.Text>
+                        {area.areaType}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Link>
+    )
+}
--- client/src/pages/departments/Departments.jsx
+++ client/src/pages/departments/Departments.jsx
@@ -4,6 +4,11 @@ import Cookies from 'universal-cookie';
 import jwt_decode from 'jwt-decode';
 
 import { Sidebar } from '../sidebar/Sidebar';
+import { DepartmentCard } from '../../components/DepartmentCard';
+
+import flaserIcon from '../../images/flaserIcon.png';
+import formingIcon from '../../images/formingIcon.png';
+
 
 export const Departments = () => {
     const cookies = new Cookies();
@@ -17,9 +22,68 @@ export const Departments = () => {
             'engineering': false,
         };
     }
+    
+    const areas = [
+        {
+            area: 'Fixture Laser',
+            link: '/fixturelaser',
+            nest: 0,
+            image: 'flaser',
+            areaType: 'production',
+        },
+        {
+            area: 'Forming',
+            link: '/forming',
+            nest: 0,
+            image: 'forming',
+            areaType: 'production',
+        },
+        {
+            area: 'Laser',
+            link: '/laser',
+            nest: 0,
+            image: 'laser',
+            areaType: 'production',
+        },
+        {
+            area: 'Punch',
+            link: '/punch',
+            nest: 0,
+            image: 'punch',
+            areaType: 'production',
+        },
+        {
+            area: 'Saw',
+            link: '/saw',
+            nest: 0,
+            image: 'saw',
+            areaType: 'production',
+        },
+        {
+            area: 'Shear',
+            link: '/shear',
+            nest: 0,
+            image: 'shear',
+            areaType: 'production',
+        },
+        {
+            area: 'Static Laser',
+            link: '/staticlaser',
+            nest: 0,
+            image: 'slaser',
+            areaType: 'production',
+        },
+        {
+            area: 'Tube Laser',
+            link: '/tubelaser',
+            nest: 0,
+            image: 'tlaser',
+            areaType: 'production',
+        },
+    ];
 
     const [loading, setLoading] = useState(false);
-
+    
     return (
         <div style={{ display: 'flex' }}>
             <Sidebar />
@@ -31,14 +95,13 @@ export const Departments = () => {
             :
                 <div style={{ display: 'block', width: '100%', marginLeft: '80px' }}>
                     <h1 className='text-center'>Departments</h1>
-                    <Link to='/fixturelaser'><h3 className='text-center'>Fixture Laser</h3></Link>                   
-                    <Link to='/forming'><h3 className='text-center'>Forming</h3></Link>    
-                    <Link to='/laser'><h3 className='text-center'>Laser</h3></Link>    
-                    <Link to='/punch'><h3 className='text-center'>Punch</h3></Link>                   
-                    <Link to='/saw'><h3 className='text-center'>Saw</h3></Link>                   
-                    <Link to='/shear'><h3 className='text-center'>Shear</h3></Link>                   
-                    <Link to='/staticlaser'><h3 className='text-center'>Static Laser</h3></Link>                
-                    <Link to='/tubelaser'><h3 className='text-center'>Tube Laser</h3></Link>                   
+                    <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', gap: '20px' }}>
+                        {areas.map((area, index) => {
+                            return (
+                                <DepartmentCard key={index} area={area} />
+                            )
+                        })}
+                    </div>
                 </div>
             }
         </div>
